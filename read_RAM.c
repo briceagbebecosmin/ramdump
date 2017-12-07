cb631c1107b1ce579e03c40d4aad30b333d58c92
briceag cosmin <briceagcosmin@gmail.com>
2017-12-07 20:09:12 +0200

    read more bytes reset to initial error code

--- read_RAM.c
+++ read_RAM.c
@@ -23,7 +23,7 @@ int main(int argc, const char *argv[])
   if (file_fd == NULL)
   {
     fprintf(stderr, "File %s cannot be open\n", argv[1]);
-    exit(-1);
+    exit(1);
   }
 
   address = (unsigned char *)mmap(NULL, page_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_LOCKED, fd, 1 * page_size);
@@ -36,7 +36,7 @@ int main(int argc, const char *argv[])
     }
   }
 
-  read(fd, buffer, 1024);
+  read(fd, buffer, 2048);
 
   for (size = 0; size < 1024; size++)
   {
